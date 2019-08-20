7c28029d28e5e226c14dcfb50e292e619b95e660
Ozkan Ciftci <ozkanciftci1991@gmail.com>
2019-08-21 01:47:04 +0300

    Running version - warning fix, banner, npm i etc.

--- src/main/java/com/tech/kk/monolithic/config/SecurityConfiguration.java
+++ src/main/java/com/tech/kk/monolithic/config/SecurityConfiguration.java
@@ -1,10 +1,5 @@
 package com.tech.kk.monolithic.config;
 
-import com.tech.kk.monolithic.security.*;
-import com.tech.kk.monolithic.security.jwt.*;
-
-import org.springframework.beans.factory.BeanInitializationException;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
@@ -20,6 +15,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 import org.springframework.web.filter.CorsFilter;
 import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
+
+import com.tech.kk.monolithic.security.AuthoritiesConstants;
+import com.tech.kk.monolithic.security.jwt.JWTConfigurer;
+import com.tech.kk.monolithic.security.jwt.TokenProvider;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
